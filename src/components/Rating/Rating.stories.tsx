fb0a766592f1df858297557ca22b4ad1bfefa478
vianat <vianatdev@gmail.com>
2021-02-08 17:00:09 -0800

    add storybook

---
+++ src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,7 @@
+import Rating from "../components/Rating/Rating";
+
+export default {
+    title: 'Example/Button',
+    component: Rating
+
+}
---
+++ src/components/accordion/Accordion.stories.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import {OnOff} from "./OnOff";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: 'OnOff stories',
+    component: OnOff
+};
+
+const callback = action("click")
+
+export const onTrue = () => <OnOff on={true} onChange={callback}/>;
+
+export const onFalse = () => <OnOff on={false} onChange={callback}/>;
+
+export const onChange = () => {
+    let [switchOn, setSwitch] = useState<boolean>(true);
+    return <OnOff on={switchOn} onChange={setSwitch}/>;
+}
